5e6362fe00590f860de72f1258edc01a1eb4aa57
kazu51-gh <kazu51git@gmail.com>
2023-09-15 05:16:40 +0900

    show number of books and hide books for the first time

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,7 +4,6 @@ import { books } from '../../books'
 import { basePath } from '../../../next.config'
 import Link from 'next/link';
 import { useState } from 'react';
-import { __String } from 'typescript';
 
 type book = {
   name: string;
@@ -21,6 +20,7 @@ const BASE_PATH = basePath ? basePath : '';
 export default function Home() {
   const [bookTitle, setBookTitle] = useState('');
   const [matchbooks, setMatchBooks] = useState<BookList>(books);
+  const [firstShowPage, setFirstShowPage] = useState(true);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBookTitle(e.target.value);
@@ -33,6 +33,7 @@ export default function Home() {
       return book.name.toUpperCase().indexOf(filter) !== -1;
     });
     setMatchBooks(filteredList);
+    setFirstShowPage(false);
   }
 
   return (
@@ -66,29 +67,36 @@ export default function Home() {
             onClick={handleClick}
           />
         </div>
+        {matchbooks.length === 0 &&
+          <div className='m-3'>
+            書籍が見つかりませんでした。
+          </div>
+        }
+        {!firstShowPage && matchbooks.length !== 0 &&
+          <div className='m-3'>
+            {matchbooks.length}点の書籍が見つかりました。
+          </div>
+        }
         <div className="grid grid-cols-3 m-3">
-          {matchbooks.length === 0 &&
-            <div>
-              書籍が見つかりませんでした。
-            </div>
-          }
-          {matchbooks.map((book, index) => (
-            <div key={index} className='flex flex-col w-4/5 mx-auto'>
-              <h3 className='py-1.5 my-auto text-2xl'>{book.name}</h3>
-              <Image
-                className='w-4/5'
-                src={`${BASE_PATH}${book.image}`}
-                alt='noImage'
-                width={600}
-                height={800}
-              />
-              <div className='flex flex-col mt-2 mb-4'>
-                <p>ISBN : {book.isbn}</p>
-                <p>出版 : {book.publication}</p>
-                <p>著者 : {book.author}</p>
+          {!firstShowPage &&
+            matchbooks.map((book, index) => (
+              <div key={index} className='flex flex-col w-4/5 mx-auto'>
+                <h3 className='py-1.5 my-auto text-2xl'>{book.name}</h3>
+                <Image
+                  className='w-4/5'
+                  src={`${BASE_PATH}${book.image}`}
+                  alt='noImage'
+                  width={600}
+                  height={800}
+                />
+                <div className='flex flex-col mt-2 mb-4'>
+                  <p>ISBN : {book.isbn}</p>
+                  <p>出版 : {book.publication}</p>
+                  <p>著者 : {book.author}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          }
         </div>
       </main>
     </div>
