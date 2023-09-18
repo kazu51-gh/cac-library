1bea932db6fbba42d5c84b658d1949d6da8ddb52
Oni-Men <sensyaheis@gmail.com>
2023-09-18 22:50:37 +0900

    コサイン類似度とN-Gramを用いた「あいまい性」を許した検索algorithm

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,7 +6,6 @@ import searchBooks, { BookList } from "./search";
 
 export default function Home() {
   const [bookTitle, setBookTitle] = useState('');
-  const [fuzzy, setFuzzy] = useState(false);
   const [matchbooks, setMatchBooks] = useState<BookList>(books);
   const [firstShowPage, setFirstShowPage] = useState(true);
 
@@ -15,7 +14,7 @@ export default function Home() {
   }
 
   const handleClick = () => {
-    const filteredList = searchBooks(bookTitle, true);
+    const filteredList = searchBooks(bookTitle, true); // 常にあいまい検索がONになっているが、今後切り替えるようにしてもいい
     setMatchBooks(filteredList);
     setFirstShowPage(false);
   }
@@ -30,7 +29,7 @@ export default function Home() {
           <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                   <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                   </svg>
               </div>
               <input onChange={handleChange} type="search" className="block w-full p-4 pl-10 text-sm border rounded-lg outline-none bg-primary" placeholder="Unity ゲーム開発" />
--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -5,17 +5,26 @@ export type BookList = Array<Book>;
 export type Vector = Array<number>;
 
 export default function searchBooks(input: string, fuzzy: boolean = false) : BookList {
-  const result = new Set<Book>(books);
+  let result: Set<Book>;
+  const keywords = splitByWhitespace(input.toUpperCase()).filter(x => !isBlank(x));
 
   if (fuzzy) {
-    // const keywords = splitByCharacterType(input.toUpperCase());
-    const keywords = input.toUpperCase().split(" ").filter(x => !isBlank(x));
+    const bookToSimilarity = new Map<Book, number>();
     for (const keyword of keywords) {
-      books.filter(book => consineSimirality(keyword, book.name.toUpperCase()) < 0.2)
-      .forEach(book => result.delete(book));
+      books.forEach(book => {
+        const simirality = maxSimilarityOfNGram(keyword, book.name.toUpperCase());
+        if (simirality > 0.8) {
+          bookToSimilarity.set(book, simirality);
+        }
+      });
     }
+
+    result = new Set<Book>();
+    const sorted = [...bookToSimilarity].sort((a ,b) => b[1] - a[1]);
+    console.log(sorted);
+    sorted.forEach(e => result.add(e[0]));
   } else {
-    const keywords = input.toUpperCase().split(" ").filter(x => !isBlank(x));
+    result = new Set<Book>(books);
     for (const keyword of keywords) {
       books
       .filter(book => book.name.toUpperCase().includes(keyword))
@@ -38,8 +47,45 @@ function isBlank(input: string): boolean {
   return input.trim().length == 0;
 }
 
-function splitByCharacterType(input: string): Array<string> {
+function splitByWhitespace(input: string): Array<string> {
   const result = new Array<string>();
+
+  let buffer = "";
+  let quotes = 0;
+  for (const c of input) {
+    if (c === "\"") {
+      quotes++;
+      if (quotes %2 == 0) {
+        result.push(buffer);
+        buffer = "";
+        continue;
+      }
+    }
+    if (c === " ") {
+      if (quotes % 2 == 0) {
+        result.push(buffer);
+        buffer = "";
+        continue;
+      }
+    }
+    buffer += c;
+  }
+  result.push(buffer);
+  return result;
+}
+
+function maxSimilarityOfNGram(str1: string, str2: string): number {
+  const [longStr, shortStr] = str1.length > str2.length ? [str1, str2] : [str2, str1];
+
+  let result = Number.MIN_VALUE;
+  for (let i = 0; i < longStr.length + shortStr.length; i++) {
+    const subString = longStr.substring(i, i + shortStr.length);
+    const cosine = consineSimilarity(shortStr, subString);
+    if (cosine > result) {
+      result = cosine;
+    }
+  }
+
   return result;
 }
 
@@ -62,7 +108,7 @@ function length(vec: Vector): number {
   return Math.sqrt(ss);
 }
 
-function consineSimirality(str1: string, str2: string): number {
+function consineSimilarity(str1: string, str2: string): number {
   const chars = new Set<string>((str1 + str2).split("").filter(c => !isBlank(c)));
   const vec1 = Array.from(chars).map(c => str1.includes(c) ? 1 : 0);
   const vec2 = Array.from(chars).map(c => str2.includes(c) ? 1 : 0);
